bd20bec09dc199cfd9f9ba1d2b46b326fb62cf2a
Autumn-1271 <60571326+Autumn-1271@users.noreply.github.com>
2020-02-09 18:49:17 +0530

    Adding more Routes to the API

--- app.js
+++ app.js
@@ -1,86 +1,17 @@
+const express = require('express');
+const app = express();
 
-function sayHello(name) {
-    console.log('Hello ' +  name);
-}
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
-//sayHello('Diya');
-console.log(window);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
-/*const logger = require('./logger');
+/*app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});*/
 
-logger = 1;
 
-logger.log('message');*/
-//basics
-
-/*const path = require('path');
-
-var pathObj = path.parse(__filename);
-
-console.log(pathObj);*/
-//path module
-
-/*const os = require('os');
-
-  var totalMemory = os.totalmem();
-  var freeMemory = os.freemem();
-
-//console.log('Total Memory: ' + totalMemory);
-
-// Template string
-// ES6 / ES2015 : ECMAScript 6
-
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);*/
-//os module
-
-/*const fs = require('fs');
-
-//const files = fs.readdirSync('./');
-//console.log(files);
-
-fs.readdir('.$', function(err,files){ 
-    if (err) console.log('Error', err);
-    else console.log('Result', files);
-})*/
-//fs module
-
-/*const EventEmitter = require('events');
-
-const log = require('./logger');
-const logger = new Logger();
-
-//Register a listener
-emitter.on('messageLogged', (arg) => {//e, eventArg
-    console.log('Listener called', arg);
-});
-
-Logger.log('message');
-
-// Making a noise, produce - singnalling
-
-//Raise: logging (data: message)*/
-//Event
-
-/*const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        res.write('Hello World');
-        res.end();
-    }
-});
-
-if (req.url === '/api/courses'){
-    res.write(JSON.stringify([1. 2. 3]));
-    res.end();
-}
-
-//server.on('connection', (socket) => {
-  //  console.log('New connection...')
-//})
-
-server.listen(3000);
-
-console.log('Listening on port 3000...');*/
-//HTTP
+module.exports = app;
